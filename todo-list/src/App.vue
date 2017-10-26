d08d3a39bb909f5423567140a6440f6e5a385390
Joao Crulhas <joao.crulhas@gmail.com>
2017-10-26 15:07:10 -0200

    Primeiro Commit

---
+++ todo-list/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <section class ="todoapp">
+      <header class = "header"> 
+        <h1>TAREFAS</h1>
+        <input-task></input-task>
+      </header>
+    </section>
+  </div>
+</template>
+
+<script>
+import InputTask from './components/InputTask'
+export default {
+  name: 'app',
+  components: {
+    InputTask
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todo-list/src/components/InputTask.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+        <input
+         type="text" 
+         placeholder="O que precisa ser feito?"
+         v-on:keyup.enter ="addTask" >
+ </div>
+</template>
+
+<script>
+// SLIDE 48 DO LIVRO
+import { Task } from '../models/Tasks.js'
+export default {
+  name: 'InputTask',
+  methods: {
+    addTask ($event) {
+      let value = $event.target.value
+      let task = new Task()
+      task.completed = false
+      task.title = value
+      console.log(task)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
